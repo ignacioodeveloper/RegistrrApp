ec6bbba60eabf7273236f4f2be2b93276931f81b
unknown <lu.barboza@duocuc.cl>
2021-09-18 19:38:56 -0300

    finish

--- src/app/pages/cuenta/cuenta.module.ts
+++ src/app/pages/cuenta/cuenta.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { CuentaPageRoutingModule } from './cuenta-routing.module';
-
 import { CuentaPage } from './cuenta.page';
 import { ComponentsModule } from 'src/app/components/components.module';
 
--- src/app/pages/cuenta/cuenta.page.ts
+++ src/app/pages/cuenta/cuenta.page.ts
@@ -1,5 +1,10 @@
+import { getAttrsForDirectiveMatching } from '@angular/compiler/src/render3/view/util';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { Usuario } from 'src/app/model/Usuario';
+import { NivelEducacional } from '../../model/NivelEducacional';
+import { Persona } from '../../model/Persona';
 
 @Component({
   selector: 'app-cuenta',
@@ -9,7 +14,29 @@ import { Usuario } from 'src/app/model/Usuario';
 export class CuentaPage implements OnInit {
 
   public usuario: Usuario;
-  constructor() { }
+  
+  constructor(
+    private activeroute: ActivatedRoute
+  , private router: Router
+  , private alertController: AlertController) {
+
+  // Se llama a la ruta activa y se obtienen sus parámetros mediante una subscripcion
+  this.activeroute.queryParams.subscribe(params => {       // Utilizamos expresión lambda
+    if (this.router.getCurrentNavigation().extras.state) { // Validar que tenga datos extras
+
+      // Si tiene datos extra, se rescatan y se asignan a una propiedad
+      this.usuario = this.router.getCurrentNavigation().extras.state.usuario;
+
+    } else {
+      /*
+        Si no vienen datos extra desde la página anterior, quiere decir que el usuario
+        intentó entrar directamente a la página home sin pasar por el login,
+        de modo que el sistema debe enviarlo al login para que inicie sesión.
+      */
+      this.router.navigate(['/login']);
+    }
+});
+}
 
   ngOnInit() {
   }
