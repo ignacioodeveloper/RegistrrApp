eb861d079b0a1e94a80149dbd43894d3673f75c8
unknown <lu.barboza@duocuc.cl>
2021-09-24 17:36:42 -0300

    bottom nav bar LISTOOO!

--- src/app/components/bottom-nav-bar/bottom-nav-bar.component.ts
+++ src/app/components/bottom-nav-bar/bottom-nav-bar.component.ts
@@ -7,8 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BottomNavBarComponent implements OnInit {
 
+  componentes: Componente[] = [
+    
+    {
+      icon: 'person-circle',
+      name: 'Cuenta',
+      redirectTo: '/cuenta'
+    },
+    {
+      icon: 'calendar',
+      name: 'Horario',
+      redirectTo: '/horario'
+    },
+    
+    {
+      icon: 'add',
+      name: 'Escanear',
+      
+      redirectTo: '/qrreader'
+    },
+    
+    {
+      icon: 'podium',
+      name: 'Asistencia',
+      redirectTo: '/asistencia'
+    },
+    
+    {
+      icon: 'help',
+      name: 'Ayuda',
+      redirectTo: '/ayuda'
+    },
+
+  ];
   constructor() { }
 
   ngOnInit() {}
 
+}
+
+interface Componente {
+  icon: string,
+  name: string,
+  redirectTo: string
 }
