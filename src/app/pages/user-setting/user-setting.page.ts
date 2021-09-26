93ab96b550fab0274e47c5e25d0e68f6a5dec8b4
unknown <lu.barboza@duocuc.cl>
2021-09-26 15:50:05 -0300

    infor cuenta y settings

--- src/app/pages/user-setting/user-setting.page.ts
+++ src/app/pages/user-setting/user-setting.page.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Persona } from 'src/app/model/Persona';
-import { NivelEducacional } from 'src/app/model/NivelEducacional';
 import { ActivatedRoute, Router, NavigationExtras} from '@angular/router';
-import { Usuario } from 'src/app/model/Usuario';
 
 @Component({
   selector: 'app-user-setting',
@@ -11,55 +9,41 @@ import { Usuario } from 'src/app/model/Usuario';
 })
 export class UserSettingPage implements OnInit {
 
-
-
   public persona: Persona = new Persona();
 
   constructor(
-    private activeroute: ActivatedRoute
+      private activeroute: ActivatedRoute
     , private router: Router
+  ) 
+    { 
+      this.activeroute.queryParams.subscribe(params => {
+        if (this.router.getCurrentNavigation().extras.state) { 
+          // guarda los datos en persona
+          this.persona = this.router.getCurrentNavigation().extras.state.persona;
+        } else {
 
-  ) { 
-        // Se llama a la ruta activa y se obtienen sus parámetros mediante una subscripcion
-        this.activeroute.queryParams.subscribe(params => {       // Utilizamos expresión lambda
-          if (this.router.getCurrentNavigation().extras.state) { // Validar que tenga datos extras
-    
-            // Si tiene datos extra, se rescatan y se asignan a una propiedad
-            this.persona = this.router.getCurrentNavigation().extras.state.persona;
-    
-          } else {
-            /*
-              Si no vienen datos extra desde la página anterior, quiere decir que el usuario
-              intentó entrar directamente a la página home sin pasar por el login,
-              de modo que el sistema debe enviarlo al login para que inicie sesión.
-            */
-            this.router.navigate(['/login']);
+          this.router.navigate(['/cuenta']);
           }
       });
   }
 
   ngOnInit() {
   }
-  public noPasarDatos(): void {
-    const navigationExtras: NavigationExtras = {
-      state: {
-        
-      }
-    }
-    this.router.navigate(['/cuenta', navigationExtras])
-  }
 
   public pasarDatos(): void {
-  
-
     const navigationExtras: NavigationExtras = {
       state: {
         persona: this.persona
       }
     }
     this.router.navigate(['/cuenta'], navigationExtras);
-  
   }
-  
+
+  public noPasarDatos(): void {
+    const navigationExtras: NavigationExtras = {
+      state: {}
+    }
+    this.router.navigate(['/cuenta', navigationExtras])
+  }
 
 }
